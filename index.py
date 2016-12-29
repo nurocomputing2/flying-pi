633d869d9c1c65183c20b45fe624c5cce198fc94
Nurocomputing1 <swift27@nurocomputing.onmicrosoft.com>
2016-12-29 17:59:45 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from picamera import PiCamera #get the PiCamera code
 from time import sleep
 import RPi.GPIO as GPIO
+import os
 
 camera = PiCamera() #defines he PiCamera
 
@@ -12,11 +13,22 @@ sleep(10)
 camera.start_preview()
 camera.start_recording('/home/pi/Desktop/image%s.h264' % i) # where the video is saved
 camrecording = True
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17,GPIO.IN)
-while True:
-  if (GPIO.input(17)):
-   camrecording = False
-   camera.stop_recording()
-   camera.stop_preview()
+GPIO.setmode(GPIO.BCM) #setting up gpio "stop button" From here 
+GPIO.setwarnings(False)
 
+button = 4 #Defines the button GPIO pin
+
+GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP) #To here
+
+while True:     #checks if button is pressed by constentle checking
+  button_state = GPIO.input(button)
+  if button_state == GPIO.HIGH:
+    print (" >>Recording...")
+  else:
+    print (" Stopping...>>")
+    camrecording = False #May be used in later parts - currently does nothing.
+   camera.stop_recording() #stops and saves the recording
+   camera.stop_preview()
+    sleep(0.5) #Avoids "bouncing" (de-bouncing)
+    print(" >>Restarting...")
+    os.system("poweroff") #restarts/ reboots the raspberry pi
