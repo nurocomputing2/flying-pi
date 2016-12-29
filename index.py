553ffd81bbd24e2c80606e69ce5236c54b47c0a8
Nurocomputing1 <swift27@nurocomputing.onmicrosoft.com>
2016-12-29 12:46:17 +0000

    Update index.py
    https://www.cl.cam.ac.uk/projects/raspberrypi/tutorials/robot/buttons_and_switches/

--- index.py
+++ index.py
@@ -1,29 +1,22 @@
-from picamera import PiCamera
+from picamera import PiCamera #get the PiCamera code
 from time import sleep
+import RPi.GPIO as GPIO
 
-camera = PiCamera()
+camera = PiCamera() #defines he PiCamera
 
-camera.start_preview()
+camera.start_preview() # start the preview to check how the cam records
 sleep(10)
-camera.stop_preview()
-imput = input("rotate? y or n")
-if imput = "y"
-imput = input("how much?")
-camera.rotation = imput
-camera.start_preview()
+camera.stop_preview
+print("starting recording in 10 secconds")
 sleep(10)
-camera.stop_preview()
-imput = input("Good?")
-if imput = "y"
-imput = input("Start recording?")
-if imput = "y"
 camera.start_preview()
-camera.start_recording('/home/pi/video.h264')
-camrec = True
-imput = input("Tell me when to stop(type stop)")
-while camrec = True
- if imput = "stop"
-  camrec = False
-camera.stop_recording()
-camera.stop_preview()
+camera.start_recording('/home/pi/Desktop/image%s.h264' % i) # where the video is saved
+camrecording = True
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.IN)
+while True:
+  if (GPIO.input(17)):
+   camrecording = False
+   camera.stop_recording()
+   camera.stop_preview()
 
